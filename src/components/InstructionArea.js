be0a0bc149da8e2469049a8a60d875e8200d9e86
ocelotdive <oscar1dlh@hotmail.com>
2018-12-31 14:31:50 +0000

    ammended intro

--- src/components/InstructionArea.js
+++ src/components/InstructionArea.js
@@ -27,7 +27,7 @@ class InstructionArea extends Component {
 	
 	render() {
 		
-	let basics = "When beginning to type, your fingers should always rest on the third row of keys or the 'Home Row'. With the index fingers find the small raised bumps on the F and J keys. From here spread each finger of both hands outwards so the fingers are lightly resting on the Row 3 three keys as in the following chart. The left and right Thumbs are used to control the space bar. The most important rule is DON'T LOOK AT THE KEYBOARD. To start choose a lesson and begin typing."
+	let basics = "When beginning to type, your fingers should always rest on the third row of keys or the 'Home Row'. With the index fingers find the small raised bumps on the F and J keys. From here spread each finger of both hands outwards so the fingers are lightly resting on the Row 3 three keys as in the following chart. The left and right Thumbs are used to control the space bar. The most important rule is DON'T LOOK AT your KEYBOARD. If you must look try to use the on screen keyboard to check which key is being struck. To start choose a lesson."
 	
 	
 	
@@ -166,28 +166,28 @@ class InstructionArea extends Component {
 			</article>
 			<div className="chart">
 			<div className="col">
-			<div>A</div>
-			<div>S</div>
-			<div>D</div>
-			<div>F</div>
-			<div>G</div>
-			<div>H</div>
-			<div>J</div>
-			<div>K</div>
-			<div>L</div>
-			<div>;</div>
+			<div style={{backgroundColor: "green"}}>A</div>
+			<div style={{backgroundColor: "green"}}>S</div>
+			<div style={{backgroundColor: "green"}}>D</div>
+			<div style={{backgroundColor: "green"}}>F</div>
+			<div style={{backgroundColor: "green"}}>G</div>
+			<div style={{backgroundColor: "yellow"}}>H</div>
+			<div style={{backgroundColor: "yellow"}}>J</div>
+			<div style={{backgroundColor: "yellow"}}>K</div>
+			<div style={{backgroundColor: "yellow"}}>L</div>
+			<div style={{backgroundColor: "yellow"}}>;</div>
 			</div>
 			<div className="col">
-			<div>L5</div>
-			<div>L4</div>
-			<div>L3</div>
-			<div>L2</div>
-			<div>L2</div>
-			<div>R2</div>
-			<div>R2</div>
-			<div>R3</div>
-			<div>R4</div>
-			<div>R5</div>
+			<div style={{backgroundColor: "aliceblue"}}>L5</div>
+			<div style={{backgroundColor: "aliceblue"}}>L4</div>
+			<div style={{backgroundColor: "aliceblue"}}>L3</div>
+			<div style={{backgroundColor: "aliceblue"}}>L2</div>
+			<div style={{backgroundColor: "aliceblue"}}>L2</div>
+			<div style={{backgroundColor: "aliceblue"}}>R2</div>
+			<div style={{backgroundColor: "aliceblue"}}>R2</div>
+			<div style={{backgroundColor: "aliceblue"}}>R3</div>
+			<div style={{backgroundColor: "aliceblue"}}>R4</div>
+			<div style={{backgroundColor: "aliceblue"}}>R5</div>
 			</div>
 			</div>
 			</div>
